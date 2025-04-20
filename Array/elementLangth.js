9fca7668bed88982e85e3022d8bf03d1ffb54c23
Nayem1004 <nayem@procoder.org>
2025-04-20 14:32:55 +0600

    finding name is array one latter search and array elements length

---
+++ Array/elementLangth.js
@@ -0,0 +1,5 @@
+let names = ["Mahamud", "Mohammad", "Fardin", "Shobuj", "Nayem"];
+
+for(let i = 0; i < names.length; i++){
+    console.log(names[i] + "=" + names[i].length);
+}
--- Array/nameFind.js
+++ Array/nameFind.js
@@ -1,10 +1,11 @@
 let names = ["Mahamud", "Mohammad", "Fardin", "Shobuj", "Nayem"];
 
-let find = 'Mahamud'
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i] + "=" + names[i].length);
+// }
+
 for(let i = 0; i < names.length; i++){
-    console.log(names[i]);
-    if(names[i] === find){
-        console.log('find the name' + i)
+    if(names[i].toUpperCase().startsWith("M")){
+        console.log(names[i]);
     }
 }
-
