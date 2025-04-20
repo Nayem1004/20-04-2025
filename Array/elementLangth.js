e3b441fdc32ada2eb57f6592ef2d67b44fbd62a5
Nayem1004 <nayem@procoder.org>
2025-04-20 14:46:40 +0600

    find element length

--- Array/elementLangth.js
+++ Array/elementLangth.js
@@ -1,5 +1,7 @@
 let names = ["Mahamud", "Mohammad", "Fardin", "Shobuj", "Nayem"];
 
 for(let i = 0; i < names.length; i++){
-    console.log(names[i] + "=" + names[i].length);
+    console.log(names[i]);
+    result = names[i] + "=" + names[i].length
+    console.log(result);
 }
