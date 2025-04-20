77acedf687d74b92c24fd7e84c4cbad8757e8168
Nayem1004 <nayem@procoder.org>
2025-04-20 14:34:24 +0600

    Update nameFind.js

--- Array/nameFind.js
+++ Array/nameFind.js
@@ -1,9 +1,5 @@
 let names = ["Mahamud", "Mohammad", "Fardin", "Shobuj", "Nayem"];
 
-// for(let i = 0; i < names.length; i++){
-//     console.log(names[i] + "=" + names[i].length);
-// }
-
 for(let i = 0; i < names.length; i++){
     if(names[i].toUpperCase().startsWith("M")){
         console.log(names[i]);
